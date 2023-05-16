f52186a9e72f893c69ac75f07f00604376362fdc
bder00s <105774363+bder00s@users.noreply.github.com>
2023-05-16 17:43:21 +0200

    project aangemaakt

---
+++ src/main/java/nl/novi/eindopdrachtbackend/EindopdrachtBackendApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdrachtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/eindopdrachtbackend/EindopdrachtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdrachtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
